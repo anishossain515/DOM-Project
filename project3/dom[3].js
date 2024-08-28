f96577264ac24fb357400bb614106a8addb025af
anishossain515 <amirul447700@gmail.com>
2024-08-29 00:24:30 +0600

    solve ` ` `````````` ` `

--- project3/dom[3].js
+++ project3/dom[3].js
@@ -4,7 +4,7 @@ let wordCount = document.querySelector('#wordCount')
 textInput.addEventListener("input",()=>{
     const text = textInput.value.trim();
 
-    const word = text.spilt(/\s+/).filter(word => word.length > 0);
+    const word = text.split(/\s+/).filter(word => word.length > 0);
 
     wordCount.textContent = word.length
 })
